68622d9536b2412141ba7b8d3261d26883c4d847
U-DESKTOP-JLB2FSD\cxue1 <tsit20@stuy.edu>
2021-10-16 17:47:30 -0400

    finished hw

--- list.c
+++ list.c
@@ -4,36 +4,55 @@
 #include <time.h>
 
 struct foo {
+	struct foo *next;
 	char c[20];
 	int i;
-	struct foo *next;
 	};
 
-void print_stat(struct foo *f);
-struct foo * make_foo(char s[20], int a, struct foo *ne);
+void print_list(struct foo *f);
+struct foo * insert_front(struct foo *ne, char s[20], int a);
+struct foo * free_list(struct foo *f);
 
 int main(){
-  struct foo *wood = make_foo("oak", 30, wood);
-  srand(time(NULL));
-  struct foo *random = make_foo("random", rand(), wood);
-  print_stat(wood);
-  print_stat(random);
-  free(wood->c);
-  free(wood);
-  free(wood->c);
-  free(random);
+	srand(time(NULL));
+	char a[20] = "oak";
+  struct foo *oak = insert_front(0, a, 30);
+	strcpy(a, "random");
+  struct foo *random = insert_front(oak, a, rand());
+	strcpy(a, "beech");
+  struct foo *beech = insert_front(random, a, 50);
+	strcpy(a, "holly");
+  struct foo *holly = insert_front(beech, a, 30);
+  print_list(holly);
+  free_list(holly);
+	return 0;
 }
 
-void print_stat(struct foo *f){
-  printf("wood:%s\t time in years to grow:%d\n", f->c, f->i);
+void print_list(struct foo *f){
+	if (f->next != 0){
+		printf("wood:%s\t time in years to grow:%d\n", f->c, f->i);
+		print_list(f->next);
+	}
+  else{
+		printf("wood:%s\t time in years to grow:%d\t end of list\n", f->c, f->i);
+	}
 }
 
-struct foo * make_foo(char s[20], int a, struct foo *ne){
+struct foo * insert_front(struct foo *ne, char s[20], int a){
   struct foo *n = malloc(sizeof(struct foo));
-  strncpy(n->c, s, sizeof((n->c)-1));
-  printf("%s\n", n->c);
-  strlen(n->c) = "\0";
-  n->i = a;
   n->next = ne;
+  strncpy(n->c, strcat(s,"\0"), strlen(s));
+  n->i = a;
   return n;
+}
+
+struct foo * free_list(struct foo *f){
+	if (f->next != 0){
+		struct foo *ne = f->next;
+		free(f);
+		free_list(ne);
+	}
+	else{
+		free(f);
+	}
 }
